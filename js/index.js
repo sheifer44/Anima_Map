ebfe75ef468a4cec4039d047e5853676761f21e1
sheifer44 <omarrodriguezv@gmail.com>
2018-02-11 21:44:47 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$('#toolbar .hamburger').on('click', function() {
+  $('#toolbar .hamburger').parent().toggleClass('open');
+});
+
+$('#toolbar2 .hamburger2').on('click', function() {
+  $('#toolbar2 .hamburger2').parent().toggleClass('open2');
+});
